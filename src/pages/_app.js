7d61ec95ef24e15b935e76bacec31708c9ce3796
Muhammad Hanan <hanan.ppqita@gmail.com>
2023-11-01 07:45:36 +0700

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
+import "@/styles/loginTemplate.css";
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
